7701a0e4cd85b6efe0f8487bc59645caf99085e7
marslan-786 <marslansalfias@gmail.com>
2026-01-08 15:10:13 +0500

    Add files via upload

--- config.go
+++ config.go
@@ -15,10 +15,10 @@ var Config = struct {
 		"120363421896353312@newsletter",
 	},
 	OTPApiURLs: []string{
-		"https://kamina-otp.up.railway.app/mait/sms",
-		"https://kamina-otp.up.railway.app/d-group/sms",
 		"https://web-production-b717.up.railway.app/api?type=sms",
+		"https://kamina-otp.up.railway.app/d-group/sms",
 		"https://kamina-otp.up.railway.app/npm-neon/sms",
+		"https://kamina-otp.up.railway.app/mait/sms",
 	},
-	Interval: 3,
+	Interval: 1,
 }
