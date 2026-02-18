613136a469984159285287fc9b33f100eee7cb87
marslan-786 <marslansalfias@gmail.com>
2026-02-18 20:08:04 +0500

    Update OTP channel ID and API URL in config.go
    Removed an old OTP channel ID and updated the API URL for the OTP service.

--- config.go
+++ config.go
@@ -12,10 +12,9 @@ var Config = struct {
 	OTPChannelIDs: []string{
 		"120363423562861659@newsletter",
 		"120363421646654726@newsletter",
-		"120363421896353312@newsletter",
 	},
 	OTPApiURLs: []string{
-		"https://api-node-js-new-production-b09a.up.railway.app/api?type=sms",
+		"https://api-kami-nodejs-production-a53d.up.railway.app/api?type=sms",
 				"https://api-kami-nodejs-production-a53d.up.railway.app/api?type=sms",
 		"https://kami-api.up.railway.app/npm-neon/sms",
 		"https://kami-api.up.railway.app/mait/sms",
