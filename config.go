a103496604256a97f438642b31e8ca9b39772403
marslan-786 <marslansalfias@gmail.com>
2026-02-18 20:12:03 +0500

    Update config.go

--- config.go
+++ config.go
@@ -14,8 +14,11 @@ var Config = struct {
 		"120363421646654726@newsletter",
 	},
 	OTPApiURLs: []string{
+		"https://api-kami-nodejs-production-a53d.up.railway.app/api?type=sms",
+		"https://kami-api.up.railway.app/d-group/sms",
 		"https://kami-api.up.railway.app/npm-neon/sms",
 		"https://kami-api.up.railway.app/mait/sms",
+		"https://api-node-js-new-production-b09a.up.railway.app/api?type=sms",
 	},
 	Interval: 6,
 }
